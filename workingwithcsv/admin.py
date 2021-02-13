f0d031aae0104d647ec4c0a986a4cbd138cd79b8
Patel_Tanmay98 <patelth1998@gmail.com>
2021-02-13 20:44:49 +0530

    Add files via upload

---
+++ workingwithcsv/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from workingwithcsv.models import *
+
+admin.site.register(Country)
+admin.site.register(State)
---
+++ workingwithcsv/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WorkingwithcsvConfig(AppConfig):
+    name = 'workingwithcsv'
